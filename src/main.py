be93e1b6bff051e805b6e8a837f8f5c11eb1c96b
OtaviOuu <conta.otaviou@gmail.com>
2024-10-07 21:59:39 -0300

    fix login

--- src/main.py
+++ src/main.py
@@ -4,16 +4,18 @@ from scrapy.http import Response
 from scrapy.crawler import CrawlerProcess
 import json
 from collections import defaultdict
+from colorama import Style
+from pystyle import Colors, Colorate
 
 
 class UniversoNarrado(Spider):
     name = "matematica"
-    
+
     final_data = defaultdict(dict)
     contador = 0
 
     custom_settings = {
-        'CONCURRENT_REQUESTS': 64,  # Altere o número conforme necessário
+        "CONCURRENT_REQUESTS": 64,
     }
 
     def start_requests(self) -> Iterable[Request]:
@@ -22,34 +24,52 @@ class UniversoNarrado(Spider):
             "Referer": "https://cursos.universonarrado.com.br/login",
             "Origin": "https://cursos.universonarrado.com.br",
             "Host": "cursos.universonarrado.com.br",
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
         }
-    
+
+        print("\033[H\033[J", end="")
         body = {
-            "email": "arthurfelixrocha243@gmail.com",
-            "password": "Nce9fnNy*",
+            "email": str(input("Email: ")),
+            "password": str(input("Senha: ")),
             "hash": "",
-            "remember": True
+            "remember": True,
         }
 
-
-        yield Request(url="https://cursos.universonarrado.com.br/auth", method="POST", body=json.dumps(body), headers=headers, callback=self.extractToken)
+        yield Request(
+            url="https://cursos.universonarrado.com.br/auth",
+            method="POST",
+            body=json.dumps(body),
+            headers=headers,
+            errback=self.wrongCredentials,
+            callback=self.extractToken,
+        )
 
     def extractToken(self, response: Response):
-        token = response.headers.getlist('Set-Cookie')[1].decode("utf-8").split(";")[0].split("=")[-1]
-        
+
+        token = (
+            response.headers.getlist("Set-Cookie")[1]
+            .decode("utf-8")
+            .split(";")[0]
+            .split("=")[-1]
+        )
+
         headers = {
             "x-auth-token": token,
             "Referer": "https://cursos.universonarrado.com.br/lesson/detail/9/7049",
             "Host": "cursos.universonarrado.com.br",
         }
-        
-        self.logger.info(f"Token: {token}")
-        yield Request(url="https://cursos.universonarrado.com.br/admin/v2/course/9", headers=headers, callback=self.extractModules, cb_kwargs={"token": token})
 
-        
+        print("\033[H\033[J", end="")
+        self.logger.info(f"Token: {token}")
+        yield Request(
+            url="https://cursos.universonarrado.com.br/admin/v2/course/9",
+            headers=headers,
+            callback=self.extractModules,
+            cb_kwargs={"token": token},
+        )
+
     def extractModules(self, response: Response, token):
-        modules = response.json()['modules']
+        modules = response.json()["modules"]
 
         headers = {
             "x-auth-token": token,
@@ -60,8 +80,13 @@ class UniversoNarrado(Spider):
         self.logger.info(token)
         for module in modules:
             ID = module["id"]
-            yield Request(url=f"https://cursos.universonarrado.com.br/admin/v2/module/{ID}", headers=headers, callback=self.extractLessons, cb_kwargs={"videoID": ID, "token":token})
-            
+            yield Request(
+                url=f"https://cursos.universonarrado.com.br/admin/v2/module/{ID}",
+                headers=headers,
+                callback=self.extractLessons,
+                cb_kwargs={"videoID": ID, "token": token},
+            )
+
     def extractLessons(self, reponse: Response, videoID, token):
         lessons = reponse.json()["lessons"]
 
@@ -70,38 +95,41 @@ class UniversoNarrado(Spider):
             "Referer": "https://cursos.universonarrado.com.br/lesson/detail/9/7049",
             "Host": "cursos.universonarrado.com.br",
         }
-        
+
         for lesson in lessons:
-            lessonID = lesson['id']
+            lessonID = lesson["id"]
 
-
-            yield Request(url=f"https://cursos.universonarrado.com.br/admin/v2/lesson/{lessonID}", callback=self.extractLessonsURL, headers=headers)
-
+            yield Request(
+                url=f"https://cursos.universonarrado.com.br/admin/v2/lesson/{lessonID}",
+                callback=self.extractLessonsURL,
+                headers=headers,
+            )
 
     def extractLessonsURL(self, response: Response):
         data = response.json()
 
-        
-
         moduleID = str(data["module"]["id"])
         lessonID = str(data["id"])
 
-        module = moduleID +" "+ data["module"]["title"]
-        title = lessonID +" "+ data["title"]
+        module = moduleID + " " + data["module"]["title"]
+        title = lessonID + " " + data["title"]
         link = data["library"]["link"]
 
         self.final_data[module][title] = link
-        
+
         with open("fisica.json", "w", encoding="utf-8") as f:
             self.contador += 1
+            print("\033[H\033[J", end="")
             print(self.contador)
             json.dump(self.final_data, f, ensure_ascii=False, indent=4, sort_keys=True)
 
-
+    def wrongCredentials(self, _failure):
+        print("\033[H\033[J", end="")
+        input(Colorate.Horizontal(Colors.rainbow, "Dados errados..."))
+        print(Style.RESET_ALL)
 
 
 process = CrawlerProcess()
 
 process.crawl(UniversoNarrado)
 process.start()
-
